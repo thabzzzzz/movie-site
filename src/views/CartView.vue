ba29996c557fffac55beebb124381e7dbb1975ae
thabzzzzz <tmosenohi99@gmail.com>
2024-01-26 04:33:23 +0200

    no message

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,45 +1,58 @@
 <template>
-    <div>
-      <h2 class="line-divider font-2xl"><span class="">Your Cart</span></h2>
-      <h3>{{ itemsHeading }}</h3>
-      <ul>
-        <li v-for="cartItem in cartItems" :key="cartItem.product.id">
-          <p v-if="cartItem">{{ cartItem.product.title }} ({{ cartItem.quantity }}) </p>
-          <p v-else>Movie information not available</p>
-        </li>
-      </ul>
-      
-    </div>
-  </template>
-  
-  <script setup>
-  import { useCartStore } from '../store/cartStore';
-  import Footer from '../components/Footer.vue';
-  const cartStore = useCartStore();
-  const cartItems = cartStore.cart;
-  </script>
+  <div>
+    <h2 class="line-divider font-2xl">
+      <span class="">Your Cart</span>
+    </h2>
 
-  <style scoped>
-  .line-divider{
+    <h3>
+      {{ cartItemCount }} {{ itemLabel }}
+    </h3>
+
+    <ul>
+      <li v-for="cartItem in cartItems" :key="cartItem.product.id">
+        <p v-if="cartItem">{{ cartItem.product.title }} ({{ cartItem.quantity }}) </p>
+        <p v-else>Movie information not available</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { useCartStore } from '../store/cartStore';
+import { ref, onMounted, computed } from 'vue';
+import Footer from '../components/Footer.vue';
+
+const cartStore = useCartStore();
+const cartItems = cartStore.cart;
+
+const cartItemCount = computed(() => {
+  return cartStore.cart.length;
+});
+
+const itemLabel = computed(() => {
+  return cartItemCount.value === 1 ? 'Item' : 'Items';
+});
+</script>
+
+<style scoped>
+.line-divider {
   margin-bottom: 40px;
 }
-    h2 {
-   width: 100%; 
-   text-align: center; 
-   border-bottom: 4px solid #000; 
-   line-height: 0.1em;
-   margin: 10px 0 20px; 
-  
-} 
 
-h2 span { 
-    background:#fff; 
-    padding:0 10px; 
-    font-size: 40px;
-    top:5px;
-    position: relative;
-    font-family: satoshi;
-    
+h2 {
+  width: 100%;
+  text-align: center;
+  border-bottom: 4px solid #000;
+  line-height: 0.1em;
+  margin: 10px 0 20px;
 }
-  </style>
-  
+
+h2 span {
+  background: #fff;
+  padding: 0 10px;
+  font-size: 40px;
+  top: 5px;
+  position: relative;
+  font-family: satoshi;
+}
+</style>
